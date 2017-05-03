c4243399310b139795eea2000fce02ea7980030a
leenos <leenos92@gmail.com>
2017-05-03 22:19:32 +0200

    functionality for houres + test

--- src/main/java/BerlinClock.java
+++ src/main/java/BerlinClock.java
@@ -7,4 +7,26 @@ public class BerlinClock {
             return "Y";
         return "O";
     }
+
+    public String getHours(int i) {
+        i=i/5;
+        String light="";
+        switch (i) {
+            case 1:
+                light= "YOOO";
+                break;
+            case 2:
+                light= "YYOO";
+            break;
+            case 3:
+                light= "YYYO";
+            break;
+            case 4:
+                light= "YYYY";
+            break;
+            default:
+                light= "OOOO";
+        } ;
+       return light;
+    }
 }
--- src/test/java/BerlinClockTest.java
+++ src/test/java/BerlinClockTest.java
@@ -22,6 +22,21 @@ public class BerlinClockTest {
         assertEquals("O",berlinClock.getSecond(19));
         assertEquals("Y",berlinClock.getSecond(0));
     }
+    @Test
+    public void testHourRowShouldHave4Lamps(){
+        assertEquals(4,berlinClock.getHours(12).length());
 
+
+    }
+    @Test
+    public void testRedLampEvery5hours(){
+        assertEquals("YOOO", berlinClock.getHours(5));
+        assertEquals("YYOO", berlinClock.getHours(10));
+        assertEquals("YYYO", berlinClock.getHours(15));
+        assertEquals("YYYY", berlinClock.getHours(20));
+        assertEquals("YYYY", berlinClock.getHours(23));
+
+
+    }
 
 }
